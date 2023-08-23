33fdbb0908f5abaa4ed93305b7afb84183b0f5be
MOQN <moonjunghyun@gmail.com>
2023-08-24 02:57:36 +0800

    upload gatsby initial files

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ml5-website-test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
